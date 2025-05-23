5f39d1176d27bc0b1cce1d92afb0f7b12fd0f8c0
Rushikumar-06 <chundururushikumar@gmail.com>
2025-05-23 20:01:13 +0530

    added error message in login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -13,7 +13,8 @@ function Login() {
   const randomNumber = Math.floor(Math.random() * 9000+1000);
   const otpRef = useRef();
   const [resendOtp, setResendOtp] = useState(false);
-    const { setLoginStatus } = useContext(loginContext);
+  const { setLoginStatus } = useContext(loginContext);
+  const [errormsg, setErrorMsg] = useState(false);
     useEffect(() => {
         otpRef.current = randomNumber;
     }, [resendOtp]);
@@ -26,7 +27,19 @@ function Login() {
   }
   return (
     <div className="login-background">
-        {!otpStatus ? <div className="login-container">
+        {!otpStatus ? 
+        <form onSubmit={(e) => {
+                e.preventDefault();
+                if (isValidMobile && parseInt(mobile)== mobile) {
+                    alert(`${otpRef.current} is your OTP`);
+                    setOtpStatus(true);
+                }
+                else{
+                    setErrorMsg(true);
+                    setMobile("")
+                }
+            }}>
+            <div className="login-container">
         <h2 className="login-heading">Login with mobile number</h2>
         <input
             type="text"
@@ -36,21 +49,20 @@ function Login() {
             onChange={(e) => setMobile(e.target.value)}
             className="login-input"
         />
+        {errormsg ? <p className='error-msg'>Please enter a valid mobile number</p> : null}
         <button
             disabled={!isValidMobile}
             className={`send-otp-button ${isValidMobile ? 'login-active' : 'login-disabled'}`}
-            onClick={() => {
-                if (isValidMobile) {
-                    alert(`${otpRef.current} is your OTP`);
-                    setOtpStatus(true);
-                }
-            }}
+            type="submit"
         >
             Send OTP
         </button>
         <button className='send-otp-button'><Link to="/" className='without-login'>Continue without login</Link></button>
-        </div>:
-        <div className="login-container">
+        </div>
+        </form>
+        :
+        <form  onSubmit={handleOtpSubmit}>
+            <div className="login-container">
             <input
             type="text"
             maxLength="4"
@@ -58,7 +70,7 @@ function Login() {
             className="login-input"
             onChange={(e) => setOtp(e.target.value)}
         />
-        <button className={`send-otp-button ${isValidMobile ? 'login-active' : 'login-disabled'}`} onClick={handleOtpSubmit}>Submit</button>
+        <button className={`send-otp-button ${isValidMobile ? 'login-active' : 'login-disabled'}`}type="submit" >Submit</button>
         <div className='otp-btn-container'>
             <button onClick={() => {
                 alert(`${otpRef.current} is your OTP`);
@@ -73,6 +85,7 @@ function Login() {
             }}>change number</button>
         </div>
         </div>
+        </form>
         
         }
     </div>
